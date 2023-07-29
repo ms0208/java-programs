1fd5836501c58652396248a09b5bca837d6789e1
Manav Sharma <102479672+ms0208@users.noreply.github.com>
2023-07-30 00:48:52 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -3,11 +3,17 @@ class Addition {
     public static void main(String[] args){
         
         Scanner sc = new Scanner (System.in); // scanner class and object 
+        
         System.out.println("Enter a number 1");
+        
         int a = sc.nextInt(); // input scannar
+        
         System.out.println("Enter a number 2");
+        
         int b = sc.nextInt();
+        
         System.out.println("Sum is");
+        
         System.out.println(a+b);
         
 
